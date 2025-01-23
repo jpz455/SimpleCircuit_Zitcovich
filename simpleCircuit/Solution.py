07c5f58eb88ea0aa7b3da75d41bac43be8703e8b
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-01-23 09:47:49 -0500

    Milestone 4 Done

--- simpleCircuit/Solution.py
+++ simpleCircuit/Solution.py
@@ -5,20 +5,27 @@ class Solution:
         self.circuit = circuit
 
     def do_power_flow(self):
+       #assign vsource element
         v_source = self.circuit.vsource
+
+       #get resistance of total circuit by accessing first element of resistor and load dictionaries
         series_resistor = list(self.circuit.resistors.values())[0]
         load_resistor = list(self.circuit.loads.values())[0]
-
         r_total = series_resistor.r + (1/load_resistor.g)
 
+       #calculate circuit current
         self.circuit.i = v_source.v/r_total
 
+       #set nodal voltages
+       #bus a is connected to source voltage
         bus_a = self.circuit.buses[v_source.bus1]
         bus_a.set_bus_voltage(v_source.v)
+
+       #access bus b
         bus_b = self.circuit.buses[list(self.circuit.buses.keys())[1]]
+
+       #solve for b using voltage drop across series resistor
         bus_b.set_bus_voltage(bus_a.voltage - self.circuit.i * series_resistor.r)
 
-        self.circuit.print_nodal_voltage()
-        self.circuit.print_circuit_current()
 
 
--- simpleCircuit/test.py
+++ simpleCircuit/test.py
@@ -13,17 +13,15 @@ bus_a = Bus("BusA")
 bus_b = Bus("BusB")
 my_circuit.add_bus(bus_a)
 my_circuit.add_bus(bus_b)
-
 v_source = VSource("VSource1","BusA",100)
 my_circuit.add_vsource_element(v_source.name,v_source.bus1,v_source.v)
-
 resistor_ab = Resistor("Rab","BusA","BusB",5)
 my_circuit.add_resistor_element(resistor_ab.name,resistor_ab.bus1,resistor_ab.bus2,resistor_ab.r)
-
 load_b = Load("Lb","BusB",2000,100)
 my_circuit.add_load_element(load_b.name,load_b.bus1,load_b.p,load_b.v)
-
 solution = Solution(my_circuit)
 solution.do_power_flow()
+my_circuit.print_nodal_voltage()
+my_circuit.print_circuit_current()
 
 
