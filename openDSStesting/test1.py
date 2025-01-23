e773adc058c1454c4b924cbba70d91d9f18e3669
jpz455 <156132550+jpz455@users.noreply.github.com>
2025-01-23 16:47:36 -0500

    Delete openDSStesting directory

--- openDSStesting/test1.py
+++
@@ -1,9 +0,0 @@
-import py_dss_interface
-
-dss = py_dss_interface.DSS()
-
-dss.text("clearall")
-dss.text(r"compile C:\Users\jilli\OneDrive - University of Pittsburgh\Desktop\ECE 1893\OpenDSS_Basics\13Bus\IEEE13Nodeckt.dss")
-dss.text("solve")
-dss.text("Show Voltage LN Nodes")
-dss.text("FormEdit Line.LINE1")
